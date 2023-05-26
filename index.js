065665f04f2076c7d16acb0411d8622f436c2ce5
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-05-26 10:17:18 -0400

    debugged and reorganized code. Added options for colour, constructor for the logo, and a switch function in order to render the user input shape.

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+function getColorCode(colorName) {
+  switch (colorName) {
+    case 'blue':
+      return '#0000FF';
+    case 'pink':
+      return '#FFC0CB';
+    case 'yellow':
+      return '#FFFF00';
+    case 'green':
+      return '#008000';
+    case 'red':
+      return '#FF0000';
+    case 'black':
+      return '#000000';
+  }
+}
+
 class Logo {
   constructor() {
     this.text = '';
     this.shape = '';
+    this.shapeColor = '';
+    this.fontColor = '';
   }
   render() {
+    const shapeColorCode = getColorCode(this.shapeColor);
+    const fontColorCode = getColorCode(this.fontColor);
     // Create the SVG markup based on the selected shape
     let svg = '';
     switch (this.shape) {
       case 'circle':
-        svg = `<svg width="100" height="100"><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="transparent" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="24">${this.text}</text></svg>`;
+        svg = `<svg version="1.1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+          <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="${shapeColorCode}" />
+          <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="24" fill="${fontColorCode}">${this.text}</text>
+        </svg>`;
         break;
       case 'square':
-        svg = `<svg width="100" height="100"><rect x="10" y="10" width="80" height="80" stroke="black" stroke-width="3" fill="transparent" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="24">${this.text}</text></svg>`;
+        svg = `<svg version="1.1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+          <rect x="10" y="10" width="80" height="80" stroke="black" stroke-width="3" fill="${shapeColorCode}" />
+          <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="24" fill="${fontColorCode}">${this.text}</text>
+        </svg>`;
         break;
       case 'triangle':
-        svg = `<svg width="100" height="100"><polygon points="50,10 90,90 10,90" stroke="black" stroke-width="3" fill="transparent" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="24">${this.text}</text></svg>`;
+        svg = `<svg version="1.1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+          <polygon points="50,10 90,90 10,90" stroke="black" stroke-width="3" fill="${shapeColorCode}" />
+          <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="24" fill="${fontColorCode}">${this.text}</text>
+        </svg>`;
         break;
     }
     // Save the SVG markup to a file
@@ -64,17 +95,19 @@ function writeToFile(fileName, data) {
     }
   });
 }
-function init() {
+function generateSvg() {
   inquirer
     .prompt(questions)
     .then(answers => {
       const logo = new Logo();
       logo.text = answers.initials;
       logo.shape = answers.shape;
+      logo.fontColor = answers.fontColor;
+      logo.shapeColor = answers.shapeColor;
       logo.render();
     })
     .catch(error => {
       console.error('Error:', error);
     });
 }
-init();
+generateSvg();
